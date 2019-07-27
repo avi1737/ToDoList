df382848f8b94510e3f282f32f109842d07f95fc
husain <syedrasique718@gmail.com>
2019-07-27 22:58:23 +0530

    second changes in python by husain

--- python/bill_data_extractor.py
+++ python/bill_data_extractor.py
@@ -1,17 +1,10 @@
 import sys
 import os
-sys.path.append(os.getcwd())
+sys.path.append(os.getcwd()+"/intp/python")
 import pandas as pd
-try:
-	import pdfplumber
-except :
-	os.system("pip3 install pdfplumber")
-	import pdfplumber
 import config as cfg
-pdf_file = sys.argv[-1]
-pdf = pdfplumber.open(pdf_file)
-pages= pdf.pages[0]
-table = pages.extract_tables()
-df = pd.DataFrame(table[1:], columns=table[0])
 
+df = pd.read_csv(os.getcwd()+"/intp/python/U031998124_111687.csv")
+df['Available'] = df['Qty']
+df
 #df.to_sql('buying_medicine' , conn , if_exists = append)
