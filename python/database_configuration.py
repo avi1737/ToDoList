1fd747fd1c0bb6b4dc59b901f5a3fa83f19eb61e
husain <syedrasique718@gmail.com>
2019-08-03 00:04:25 +0530

    changes made by husain

--- python/database_configuration.py
+++ python/database_configuration.py
@@ -1,6 +1,11 @@
+#!/usr/bin/env python3
+import os
+import sys
+sys.path.append(os.getcwd())
 import pymysql as sql
+
 import config as cfg
-print(cfg.mysql_medihelp)
+
 
 conn =  sql.connect(host = cfg.mysql_medihelp['server'],username= cfg.mysql_medihelp['username'] , password = cfg.mysql_medihelp['password'],cursorclass=sql.cursors.DictCursor)
 cursor=conn.cursor()
